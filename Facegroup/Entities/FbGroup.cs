8cead51d11b3f083986c20c3c5eb94fdec7759e8
Alex <alex@urghh.net>
2017-08-23 16:49:54 +0300

    update reading groups from group list

--- Facegroup/Entities/FbGroup.cs
+++ Facegroup/Entities/FbGroup.cs
@@ -13,6 +13,7 @@ namespace Facegroup
         {
             get
             {
+                if (string.IsNullOrEmpty(GroupUnreadText)) return 0;
                 int result = int.Parse(Regex.Match(GroupUnreadText, @"\d+").Value);
                 return result;
             }
@@ -21,8 +22,16 @@ namespace Facegroup
         public FbGroup(IWebElement groupEl)
         {
             GroupName = groupEl.FindElement(By.TagName("a")).Text;
-            GroupUrl = groupEl.FindElements(By.TagName("a"))[1].GetAttribute("href");
-            GroupUnreadText = groupEl.FindElements(By.TagName("a"))[1].Text;
+            GroupUrl = groupEl.FindElement(By.LinkText(GroupName)).GetAttribute("href");
+            var links = groupEl.FindElements(By.TagName("a"));
+            foreach(var link in links)
+            {
+                if (link.Text.Contains("непрочетен"))
+                {
+                    GroupUnreadText = link.Text;
+                    break;
+                }
+            }
         }
 
         public override string ToString()
